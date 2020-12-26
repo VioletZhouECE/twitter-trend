2dbecd908af2fbb8abf1a3597c9a08df1eb087ef
VioletZhouECE <x375zhou@uwaterloo.ca>
2020-12-25 19:31:15 -0500

    connect to sample stream endpoint

--- tweepy_streaming.py
+++ tweepy_streaming.py
@@ -1,5 +1,6 @@
 from settings.config_dev import settings
 import tweepy
+import time 
 
 CUSTOMER_KEY = settings["CUSTOMER_KEY"]
 CUSTOMER_SECRET = settings["CUSTOMER_SECRET"]
@@ -10,5 +11,20 @@ auth = tweepy.OAuthHandler(CUSTOMER_KEY, CUSTOMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
-for tweet in tweepy.Cursor(api.search, q='tweepy').items(10):
-    print(tweet.text)
+
+class StreamListener(tweepy.StreamListener):
+    def on_data(self, data):
+	    print(data)
+	    return True
+
+    def on_status(self, status):
+        print(status.text)
+        return False
+
+streamListener = StreamListener()
+twitterStream = tweepy.Stream(auth=api.auth, listener=streamListener)
+
+#test: stream for 10s
+twitterStream.sample()
+time.sleep(10)
+twitterStream.disconnect()
